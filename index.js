92081ffd002ab4522f1dd1a27d06918b2e8f28a9
Alyona Goncharova <bestliona@gmail.com>
2017-05-08 12:06:14 +0300

    refactoring

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 "use strict";
 const fs = require('fs');
 
-const getMemory = setInterval(()=>{
+const getMemory = setInterval(() => {
     console.log((process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2), 'mb');
 }, 100);
 getMemory.unref();
@@ -9,69 +9,41 @@ getMemory.unref();
 getStatistics('input.txt');
 function getStatistics(filePath) {
 
-    const alphabet = 'a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z'.split(',');
+    const alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('');
 
-    function getContent() {
-        return new Promise((resolve, reject)=>{
-            fs.readFile(filePath, (err, data)=>{
-                if(err) reject(err);
+    const getContent = function() {
+        return new Promise((resolve, reject) => {
+            fs.readFile(filePath, (err, data) => {
+                if (err) reject(err);
                 else resolve(data);
             });
         });
-    }
+    };
 
-    function generateLettersFile(data) {
-        return new Promise((resolve, reject)=>{
-            data = data.toString();
-            let output_summary = [];
+    const generateLettersFile = function(data) {
+        return new Promise((resolve, reject) => {
 
-            alphabet.forEach((letter, i , arr)=>{
-                console.log('Start letter', letter);
-                let positions = [];
-                let count = 0;
-                let currentPosition = -1;
-                let fileName = 'output_'+letter + '.txt';
-                fs.open(fileName,'w',(err, fd)=>{
+            let alphabetPromises = alphabet.reduce((result, letter) => {
+                return getLetterPromise(data.toString(), letter, result);
+            }, []);
 
-                    console.log('Write data to file', fileName);
-                    if(err) reject(err);
-
-                    while (true){
-                        let i = data.indexOf(letter, currentPosition + 1);
-                        if(i < 0) break;
-                        else {
-                            currentPosition = i;
-                            positions.push(i);
-                            count++;
-                        }
-                    }
-
-                    fs.write(fd, positions.toString(),(err)=> {
-                        if(err) reject(err);
-                        console.log('Data was written to file:', fileName);
-                        if(i === arr.length - 1 ){
-                            resolve(output_summary);
-                        }
-                    });
-                    output_summary.push(letter + '=' + count);
-                });
-
-                console.log('Letter ' + letter +' completed');
-            });
-
+            Promise.all(alphabetPromises)
+                .then((output_summary) => resolve(output_summary))
+                .catch((err) => reject(err));
         });
-    }
+    };
 
     const writeSummaryFile = function (output_summary) {
-        return new Promise((resolve, reject)=>{
+        return new Promise((resolve, reject) => {
 
             console.log('All letters completed. Total letters processed: ' + output_summary.length);
+            fs.open('output_summary.txt', 'w', (err, fd) => {
 
-            fs.open('output_summary.txt',  'w' , (err, fd)=>{
                 console.log('Write data to file: output_summary.txt');
-                if(err) reject(err);
-                fs.write(fd,output_summary.join('\n'), (err)=>{
-                    if(err) reject(err);
+                if (err) reject(err);
+
+                fs.write(fd, output_summary.join('\n'), (err) => {
+                    if (err) reject(err);
                     console.log('Data was written to file: output_summary.txt');
                 })
             });
@@ -81,5 +53,46 @@ function getStatistics(filePath) {
     return getContent()
         .then(generateLettersFile)
         .then(writeSummaryFile)
-        .catch((err)=> console.log(err));
+        .catch((err) => console.log(err));
+}
+
+function getLetterPromise(data, letter, arrayOfPromises) {
+    arrayOfPromises.push( new Promise((resolve, reject) => {
+        console.log('Start letter', letter);
+
+        let fileName = 'output_' + letter + '.txt';
+        let positions = getPositions(data, letter);
+
+        fs.open(fileName, 'w', (err, fd) => {
+
+            if (err) reject(err);
+            console.log('Write data to file', fileName);
+
+            fs.write(fd, positions.toString(), (err) => {
+                if (err) reject(err);
+                console.log('Data was written to file:', fileName);
+                resolve(letter + '=' + positions.length);
+            });
+        });
+
+        console.log('Letter ' + letter + ' completed');
+
+    }));
+    return arrayOfPromises;
+}
+
+function getPositions(data, letter){
+
+    let positions = [];
+    let currentPosition = -1;
+
+    while (true) {
+        let i = data.indexOf(letter, currentPosition + 1);
+        if (i < 0) break;
+        else {
+            currentPosition = i;
+            positions.push(i);
+        }
+    }
+    return positions;
 }
